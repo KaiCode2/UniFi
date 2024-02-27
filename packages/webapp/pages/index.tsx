90742f84def90465a80a3065bd179917ddb455d4
Adam Attalla <you@example.com>
2024-02-26 21:23:47 -0700

    setup frontend

--- packages/webapp/pages/index.tsx
+++ packages/webapp/pages/index.tsx
@@ -1,13 +1,91 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+// 1. build basic front end
+// 2.
+// 3.
+import { ethers } from "ethers";
+import { useMemo } from "react";
+import type { Chain, Client, Transport } from "viem";
+import { Config, useClient } from "wagmi";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+const TransferFunds = () => {
+  return (
+    <>
+      <header>
+        <h1>Omnaccounts</h1>
+        <nav>
+          <a href="#" className="active">
+            Home
+          </a>
+          <a href="#">Omniwallet</a>
+          <a href="#">About Us</a>
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              // @ts-expect-error
+              if (window.ethereum) {
+                console.log(ethers);
+                // @ts-expect-error
+                const provider = new ethers.BrowserProvider(window?.ethereum);
 
-export default IndexPage;
+                await provider.send("eth_requestAccounts", []);
+
+                const signer = provider.getSigner();
+              }
+            }}
+            className="connect-wallet-btn"
+            style={{ float: "right" }}
+          >
+            Connect Wallet
+          </button>
+        </nav>
+      </header>
+      <form>
+        <div>
+          <label htmlFor="amount1">Amount 1:</label>
+          <input
+            type="number"
+            id="amount1"
+            name="amount1"
+            min="0"
+            step="1"
+            pattern="\d*"
+            inputMode="numeric"
+            autoComplete="off"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="amount2">Amount 2:</label>
+          <input
+            type="number"
+            id="amount2"
+            name="amount2"
+            min="0"
+            step="1"
+            pattern="\d*"
+            inputMode="numeric"
+            autoComplete="off"
+          />
+        </div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            // Do nothing, button is empty
+          }}
+          className="submit-btn"
+        >
+          Exchange Funds
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default TransferFunds;
+
+// const IndexPage = () => {
+//   return (
+
+//   );
+// };
+
+// export default IndexPage;

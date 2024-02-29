8190f485d6cbca591d2189876d7d36241c21d04f
Kai Aldag <kaialdag@icloud.com>
2024-02-29 15:26:59 -0700

    WIP Across send tx

--- packages/contracts/contracts/safe/BaseModule.sol
+++ packages/contracts/contracts/safe/BaseModule.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {IAccount} from "@account-abstraction/contracts/interfaces/IAccount.sol";
 import {AcrossHookReceiver} from "../bridge/AcrossHookReceiver.sol";
 import {CCIPHookReceiver} from "../bridge/CCIPHookReceiver.sol";
+import {AcrossSender} from "../bridge/AcrossSender.sol";
 import {OmnaccountErrors as Errors} from "../interfaces/Errors.sol";
 import {AccountEntry} from "./AccountEntry.sol";
 import {ISafe} from "../interfaces/ISafe.sol";
@@ -11,7 +12,7 @@ import {UserOperation} from "@account-abstraction/contracts/interfaces/UserOpera
 import {_packValidationData} from "@account-abstraction/contracts/core/Helpers.sol";
 
 
-abstract contract BaseModule is AcrossHookReceiver, AccountEntry { //, IAccount, CCIPHookReceiver {
+abstract contract BaseModule is AcrossHookReceiver, AccountEntry, AcrossSender { //, IAccount, CCIPHookReceiver {
 
     //  ─────────────────────────────────────────────────────────────────────────────
     //  Structs

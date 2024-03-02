d39efd4e4a8c207c045f20d43a5c9326ed10a60a
Kai Aldag <kaialdag@icloud.com>
2024-03-01 21:17:34 -0700

    deploy vault from backend

--- packages/webapp/src/utils/chains.ts
+++ packages/webapp/src/utils/chains.ts
@@ -1,7 +1,29 @@
 import ethLogo from '../../public/ethLogo.png';
 import baseLogo from '../../public/baseLogo.png';
 
-export const SUPPORTED_NETWORKS = {
+export const CHAIN_ID = [
+  '0xaa36a7', // ETH Sepolia
+  '0x14a33', // Base Sepolia
+];
+
+export type ChainIds="0xaa36a7"|'0x14a33'
+
+export interface Network {
+  chainName: string;
+  logo: string;
+  token: {
+    name: string;
+    decimals: number;
+    symbol: string;
+    rpcUrl: string;
+  };
+}
+
+export interface SupportedNetworks {
+  [chainId: string]: Network;
+}
+
+export const SUPPORTED_NETWORKS: SupportedNetworks = {
   '0xaa36a7': {
     chainName: 'ETH Sepolia',
     logo: ethLogo.src,
@@ -28,3 +50,12 @@ export const selectableChains = [
   { value: '0xaa36a7', label: 'ETH Sepolia' },
   { value: '0x14a33', label: 'Base Sepolia' },
 ];
+
+export interface RpcUrls {
+  [chainId: string]: string;
+}
+
+export const RPC_URLS: RpcUrls = {
+  '0xaa36a7': process.env.SEPOLIA_RPC_URL ?? 'https://ethereum-sepolia-rpc.publicnode.com',
+  '0x14a33': process.env.BASE_SEPOLIA_RPC_URL ?? 'https://base-goerli.public.blastapi.io',
+};
--- packages/webapp/src/utils/helpers.ts
+++ packages/webapp/src/utils/helpers.ts
@@ -1,12 +1,26 @@
+import sepoliaFallbackHandler from '../../../contracts/deployments/sepolia/OmnaccountFallback.json';
+import baseSepoliaFallbackHandler from '../../../contracts/deployments/baseSepolia/OmnaccountFallback.json';
+
 export const decimalToHexChainId = (decimalChainId: number): string => {
-  return '0x' + decimalChainId.toString(16);
+  return "0x" + decimalChainId.toString(16);
 };
 
-export const DEVELOPMENT_WEBSITE_URL = 'http://localhost:3000';
+export const DEVELOPMENT_WEBSITE_URL = "http://localhost:3000";
 // TODO: change in prod to deployed URL
-export const PRODUCTION_WEBSITE_URL = 'http://localhost:3000';
+export const PRODUCTION_WEBSITE_URL = "http://localhost:3000";
 
 export const getEnvironmentWebsiteUrl = () =>
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === "production"
     ? PRODUCTION_WEBSITE_URL
     : DEVELOPMENT_WEBSITE_URL;
+
+
+interface FallbackHandlerAddresses {
+  [chainId: string]: string;
+}
+
+export const NETWORK_FALLBACK_HANDLERS: FallbackHandlerAddresses = {
+  // ETH Sepolia
+  "0xaa36a7": sepoliaFallbackHandler.address,
+  "0x14a33": baseSepoliaFallbackHandler.address,
+};
